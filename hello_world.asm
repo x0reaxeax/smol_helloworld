b11e698bd93ae43bf4048b9b1159be2625721c99
x0reaxeax <61374847+x0reaxeax@users.noreply.github.com>
2022-02-14 14:46:16 +0100

    poc

---
+++ hello_world.asm
@@ -0,0 +1,29 @@
+_start:
+    xor eax, eax        ; syscall no
+    xor ebx, ebx        ; fdno
+    xor edx, edx        ; slen
+    
+.loopeax:
+    inc eax
+    cmp eax, 4          ; sys_write = 4
+    jl .loopeax
+
+.loopedx:
+    inc edx
+    cmp edx, 11         ; strlen = 11
+    jl .loopedx
+    
+    mov ecx, 0x80480f1  ; "helloworld!"
+    int 0x80
+    
+    db 'h'
+    db 'e'
+    db 'l'
+    db 'l'
+    db 'o'
+    db 'w'
+    db 'o'
+    db 'r'
+    db 'l'
+    db 'd'
+    db '!'
